af0a9250fed2762de6f0df3ec5b3dbc409492ae5
Namit000 <99240864+Namit000@users.noreply.github.com>
2023-09-07 00:43:25 +0530

    Add files via upload

---
+++ Assignmentday3/Program7.java
@@ -0,0 +1,49 @@
+public class Program7 {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++)
+		{
+			for(int j=5;j>=1;j--)
+			{
+				if(j==i) {
+					System.out.print(" "+i);
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			for(int j=2;j<=5;j++)
+			{
+				if(j==i) {
+					System.out.print(" "+i);
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+		for(int i=4;i>=1;i--)
+		{
+			for(int j=5;j>=1;j--)
+			{
+				if(j==i) {
+					System.out.print(" "+i);
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			for(int j=2;j<=5;j++)
+			{
+				if(j==i) {
+					System.out.print(" "+i);
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+    }
+    
+}
---
+++ Assignmentday3/Programm5.java
@@ -0,0 +1,13 @@
+public class Programm5 {
+    public static void main(String[] args) {
+        for(int i=5;i>0;i--)
+		{
+			for(int j=i;j<=5;j++)
+			{
+				System.out.print(" "+j);
+			}
+			System.out.println();
+		}
+    }
+    
+}
